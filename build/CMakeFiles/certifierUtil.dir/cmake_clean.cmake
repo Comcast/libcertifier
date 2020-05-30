6eb51561d1d3c5a77a70a9d7c219f7903a441fd3
Asad Haque <asad_haque@cable.comcast.com>
2020-05-29 22:10:35 -0400

    Updated README

---
+++ build/CMakeFiles/certifierUtil.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/certifierUtil.dir/src/main.c.o"
+  "certifierUtil.pdb"
+  "certifierUtil"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/certifierUtil.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
