52c7855ae26dfbffd6404febfdb69b6a1a301d77
Bahar Limaye <Bahar_Limaye@cable.comcast.com>
2020-06-01 11:12:13 -0400

    1.  Deleted build folder.  That should have never been checked in. 2.  Deleted Untitled.code-workspace.  That should have never been checked in. 3.  Created .gitignore file

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ahaque201/Github/Comast Opensource Github/libcertifier
-# Build directory: /Users/ahaque201/Github/Comast Opensource Github/libcertifier/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(certifier-tests "/Users/ahaque201/Github/Comast Opensource Github/libcertifier/build/certifierTests")
